914a9a4434ff12b8ee0cdfd0f22728005c07afb6
rickyurvina <ricky_uc12@hotmail.com>
2021-01-10 10:58:35 -0500

    Create database

---
+++ database/migrations/2021_01_10_153509_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // codigo, nombre, descripccion, imagen para imagen utilizar paquete de laravel gestion de imagenes
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description');
+            $table->string('photo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
